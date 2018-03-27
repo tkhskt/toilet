27f18e8c1d9f4c493bbb67c5b6ccbc1f526c7194
takahashi-keita <takahashi-keita@KYCNT1407-03.local>
2018-03-27 09:39:39 +0900

    add: controller

---
+++ tables.sql
@@ -0,0 +1,44 @@
+-- ユーザーテーブル
+CREATE TABLE users (
+    `id` BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(25),
+    `password` VARCHAR(16),
+    `salt` VARCHAR(8),
+    `created_at` TIMESTAMP NOT NULL DEFAULT current_timestamp
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- トイレテーブル google apiから返されるidのカラムを追加する
+CREATE TABLE toilets (
+    `id` BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `name` TEXT,
+    `lat` DOUBLE NOT NULL,
+    `lng` DOUBLE NOT NULL,
+    `geolocation` TEXT,
+    `image_path` TEXT,
+    `description` TEXT,
+    `valuation` DOUBLE,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT current_timestamp on update current_timestamp,
+    `created_at` TIMESTAMP NOT NULL DEFAULT current_timestamp
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ユーザーとトイレの中間テーブル user_idとtoilet_idを複合主キーにする
+CREATE TABLE users_toilets (
+    `id` BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `user_id` BIGINT UNSIGNED NOT NULL,
+    `toilet_id` BIGINT UNSIGNED NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT current_timestamp
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- レビュー user_idとtoilet_idを複合主キーにする
+CREATE TABLE reviews (
+    `id` BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `toilet_id` BIGINT UNSIGNED,
+    `user_id` BIGINT UNSIGNED,
+    `valuation` DOUBLE,
+    `message` TEXT,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT DEFAULT current_timestamp on update current_timestamp,
+    `created_at` TIMESTAMP NOT NULL DEFAULT current_timestamp
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
