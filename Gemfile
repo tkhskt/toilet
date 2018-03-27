1a13bd5c5efc045e7a8e63d2a3d2bac3d89f655b
gericass <bonjovi1926@yahoo.co.jp>
2018-03-27 21:34:29 +0900

    add: devise

--- Gemfile
+++ Gemfile
@@ -5,6 +5,11 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'devise'
+gem 'omniauth-twitter'
+
+gem 'dotenv-rails'
+
 gem 'mysql2', '~> 0.4.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327013809) do
+ActiveRecord::Schema.define(version: 20180327121827) do
 
   create_table "reviews", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "toilet_id", null: false
@@ -39,6 +39,22 @@ ActiveRecord::Schema.define(version: 20180327013809) do
     t.string "icon_path"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "provider"
+    t.string "uid"
+    t.string "username"
+    t.string "image_url"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "users_toilets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
