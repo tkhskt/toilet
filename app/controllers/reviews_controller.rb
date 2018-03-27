2fbb4f5d38da6a8b0166187956a55f59b77db53a
gericass <bonjovi1926@yahoo.co.jp>
2018-03-27 21:00:36 +0900

    add: review function

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,9 @@
 class ReviewsController < ApplicationController
     def post
-        @review
+        title = params[:title]
+        message = params[:message]
+        logger.debug(title)
+    end
+    def after_update_path_for(resource)
     end
 end
