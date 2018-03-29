c8106a715a4dc0c17b560ec7361fc22c7a3d553e
gericass <bonjovi1926@yahoo.co.jp>
2018-03-29 22:06:18 +0900

    fix: レイアウト

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,19 +5,20 @@ class SearchController < ApplicationController
         keyword = params[:keyword]
         key = ENV["GOOGLE_KEY"]
         geoPoint = Toilet.getPlaceByKeyWord(keyword)
-        if geoPoint["error"].nil? then
+        if geoPoint["error"].nil? && keyword != "" then
             lat = geoPoint["coordinate"][0]["lat"][0]
             lng = geoPoint["coordinate"][0]["lng"][0]
             location = lat + "," + lng
         else 
-            @query = "#{keyword}の検索結果"
+            @query = "#{keyword}周辺のトイレ"
             @result = []
+            @topUser = User.getPopularUsers()
             render 'search/search'
             return
         end
         @topUser = User.getPopularUsers()
         @result = Toilet.getToiletList(lat,lng)
-        @query = "#{keyword}の検索結果"
+        @query = "#{keyword}周辺のトイレ"
         render 'search/search'
     end
 end
